bc248650c97b98f47312ee90f13ecf32087f823e
Gustavo Vergara <gustavovgarcia.sl@gmail.com>
2020-09-29 23:41:37 -0300

    Remove Quick and Nimble

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "2.0.0")),
-        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.1.2")),
+//        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "2.0.0")),
+//        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.1.2")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -33,7 +33,7 @@ let package = Package(
         ),
         .testTarget(
             name: "CoordinatorTests",
-            dependencies: ["Coordinator", "TestKit", "Quick", "Nimble"]
+            dependencies: ["Coordinator", "TestKit"]//, "Quick", "Nimble"]
         ),
     ]
 )
